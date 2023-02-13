b50535b2e353b1e0298cf748ee0b77836a9d09a0
Ivy Olson <ivy.olson@umontana.edu>
2023-02-12 23:24:08 -0700

    Added Sketch Comments
    explanation for the Load Bearing Log mostly

--- P5 Object HW/sketch.js
+++ P5 Object HW/sketch.js
@@ -2,22 +2,28 @@ var Kitty;
 var inputStrings = [];
 
 function preload() {
+  // Why do these have to be in the preload! Why can't they load in on construct! How you wound me p5.js!!
+
   inputStringsIdle = loadStrings("animations/idle/idle.txt");
   inputStringsWalk = loadStrings("animations/walk/walk.txt");
-  inputStrings = [inputStringsIdle, inputStringsWalk];
+  
+  inputStrings = [inputStringsIdle, inputStringsWalk]; // This condenses the input strings into an array which makes for better readability. Maybe not the most efficient, but I like it.
 }
 
 function setup() {
   createCanvas(2000, 2000);
- // imageMode(CENTER);
-  console.log("THIS SUCKS!!!!!!!!!!! WHY DOES THIS WORK!!!!!!!!!!!!!");
+
+  console.log("THIS SUCKS!!!!!!!!!!! WHY DOES THIS WORK!!!!!!!!!!!!!"); // Context: I need this log or the program breaks. No clue why or how, I just do. The Load-Bearing Tomato of this project >:(
   
   Kitty = new character(inputStrings, 100, 100);
+
 }
 
 function draw(){
+ 
   background(255);
   Kitty.tick();
+
 }
 
 
